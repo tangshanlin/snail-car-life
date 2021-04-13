0d550fdd2af8a9bf04594ed2e7c7840851406755
tangshanlin <1360166328@qq.com>
2021-04-13 19:46:08 +0800

    2021.4.13 tsl commit 19:49

--- car-user-server/car-user-web/src/main/java/com/woniu/car/UserRunStarter.java
+++ car-user-server/car-user-web/src/main/java/com/woniu/car/UserRunStarter.java
@@ -3,9 +3,11 @@ package com.woniu.car;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.woniu.car.user.web.mapper")
+@EnableFeignClients
 public class UserRunStarter {
     public static void main(String[] args) {
         SpringApplication.run(UserRunStarter.class,args);
