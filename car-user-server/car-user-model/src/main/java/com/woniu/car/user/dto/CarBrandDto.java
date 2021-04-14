89299b063e877156889dc011eb0ef1a8b93a7180
s@infor.vip <s@infor.vip>
2021-04-14 19:41:49 +0800

    20210414提交用户中心第三次

--- car-user-server/car-user-model/src/main/java/com/woniu/car/user/dto/CarBrandDto.java
+++ car-user-server/car-user-model/src/main/java/com/woniu/car/user/dto/CarBrandDto.java
@@ -1,22 +1,54 @@
 package com.woniu.car.user.dto;
 
-import com.woniu.car.user.web.domain.CarBrand;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.List;
+import java.io.Serializable;
 
 /**
- * @ClassName CarBrandDto
- * @Desc TODO
- * @Author Administrator
- * @Date 2021/4/14 17:34
- * @Version 1.0
+ * <p>
+ * 车品牌表
+ * </p>
+ *
+ * @author sushanhu
+ * @since 2021-04-14
  */
 @Data
-public class CarBrandDto {
-    String key;
-    List<CarBrand> value;
 
+
+public class CarBrandDto implements Serializable {
+
+
+
+    @ApiModelProperty(value = "id")
+    private Integer carbrandId;
+
+    @ApiModelProperty(value = "品牌名字")
+    private String carbrandName;
+
+    @ApiModelProperty(value = "图片")
+    private String carbrandImage;
+
+    @ApiModelProperty(value = "首字母")
+    private String carbrandFirstletter;
+
+    @ApiModelProperty(value = "创建时间")
+      @TableField(fill = FieldFill.INSERT)
+    private Long gmtCreate;
+
+    @ApiModelProperty(value = "修改时间")
+      @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long gmtModified;
+
+    @ApiModelProperty(value = "删除")
+    @TableLogic
+    private Integer deleted;
 
 
 }
--- car-user-server/car-user-web/src/main/java/com/woniu/car/user/web/controller/CarBrandController.java
+++ car-user-server/car-user-web/src/main/java/com/woniu/car/user/web/controller/CarBrandController.java
@@ -1,11 +1,14 @@
 package com.woniu.car.user.web.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.woniu.car.commons.core.code.ConstCode;
 import com.woniu.car.commons.core.dto.ResultEntity;
+import com.woniu.car.commons.web.util.BeanCopyUtil;
+import com.woniu.car.user.dto.CarBrandDto;
+import com.woniu.car.user.dto.CarBrandListDto;
 import com.woniu.car.user.web.domain.CarBrand;
 import com.woniu.car.user.web.service.CarBrandService;
-import io.minio.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,10 +39,20 @@ public class CarBrandController {
     @GetMapping("/select_all_brand")
     @ApiOperation(value = "查询所有车品牌的接口",notes = "<span style='color:red;'>查询所有车品牌的接口</span>")
     @ApiResponse(code = 1344,message = "查询车品牌成功")
-    public ResultEntity<List<CarBrand>> selectAllBrand(){
-        List<CarBrand> list = carBrandService.list();
-     return ResultEntity.buildListEntity(CarBrand.class).setCode(ConstCode.SELECTCARBRAND_SUCESS).setFlag(true)
-             .setMessage("查询所有车品牌成功").setData(list);
+    public ResultEntity<List<CarBrandListDto>> selectAllBrand(){
+        List<CarBrandListDto> objects = new ArrayList<CarBrandListDto>();
+        for (int i=65;i<91;i++){
+            String s = String.valueOf((char) i);
+            List<CarBrand> carband_firstletter = carBrandService.list(new QueryWrapper<CarBrand>().eq("carband_firstletter", s));
+            CarBrandListDto carBrandListDto = new CarBrandListDto();
+            carBrandListDto.setKey(s);
+            List<CarBrandDto> carBrandDtos = BeanCopyUtil.copyList(carband_firstletter, CarBrandDto::new);
+            carBrandListDto.setValue(carBrandDtos);
+            objects.add(carBrandListDto);
+
+        }
+     return ResultEntity.buildListEntity(CarBrandListDto.class).setCode(ConstCode.SELECTCARBRAND_SUCESS).setFlag(true)
+             .setMessage("查询所有车品牌成功").setData(objects);
 
     }
 
